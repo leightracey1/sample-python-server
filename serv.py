4843e96e049def11ad4cbf63901f3166648f567c
leightracey1 <ltracey203@gmail.com>
2021-05-12 13:09:07 -0400

    python & html file

---
+++ serv.py
@@ -0,0 +1,59 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+import json
+from types import SimpleNamespace
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, 'utf-8'))
+        
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        py_data = json.loads(post_data)
+        print(py_data["email"])
+        # do what ever you want here with the data recieved from the client 
+
+        # convert back to json 
+        json_data = json.dumps(py_data)
+        self._set_response()
+        # send response back to client 
+        self.wfile.write(json_data.encode('utf-8'))
+
+        # TO DO -- Error handling on post 
+
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
